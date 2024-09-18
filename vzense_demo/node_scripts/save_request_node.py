488ba259d931bc5f7b0e68e94e59c68af3f5d3ad
Iori Yanokura <ab.ioryz@gmail.com>
2024-09-19 02:41:39 +0900

    Add save request data

--- vzense_demo/node_scripts/save_request_node.py
+++ vzense_demo/node_scripts/save_request_node.py
@@ -3,6 +3,10 @@
 
 import rospy
 from std_srvs.srv import Trigger, TriggerRequest, Empty, EmptyResponse
+from pybsc import run_command
+from pybsc import make_fancy_output_dir
+import rospkg
+from pathlib import Path
 
 
 def handle_save_image_request(req):
@@ -24,11 +28,36 @@ def handle_save_image_request(req):
     return EmptyResponse()
 
 
+def handle_calibration(req):
+    rospy.loginfo("Received request to calibration.")
+    run_command('rosrun vzense_demo aggregate_calib_images.py', shell=True)
+    run_command('docker run -ti --rm --volume="$HOME/MC-Calib:/home/MC-Calib" --volume="$HOME/MC-Calib/data:/home/MC-Calib/data" --volume="$(rospack find vzense_demo)/config/calib_param_two_vzense.yaml:/home/MC-Calib/configs/calib_param_two_vzense.yaml" --volume="$(rospack find vzense_demo)/calib_results:/home/MC-Calib/data/vzense_data" bailool/mc-calib-prod bash -c "cd /home/MC-Calib/build && ./apps/calibrate/calibrate ../configs/calib_param_two_vzense.yaml"', shell=True)
+    run_command('rosrun vzense_demo set_calib_tf.py', shell=True)
+    return EmptyResponse()
+
+
+def handle_clear_data(req):
+    rospy.loginfo("Received request to clear data.")
+    rospack = rospkg.RosPack()
+    package_path = Path(rospack.get_path('vzense_demo'))
+    source_path = package_path / 'data'
+    target_path = package_path / 'data_backup'
+    backup_path = Path(make_fancy_output_dir(target_path))
+    run_command(f'mv {source_path}/* {backup_path}', shell=True)
+    return EmptyResponse()
+
+
 def main():
     rospy.init_node('save_request_node')
-    service = rospy.Service(
+    rospy.Service(
         '/save_image_request', Empty,
         handle_save_image_request)
+    rospy.Service(
+        '/calib', Empty,
+        handle_calibration)
+    rospy.Service(
+        '/clear_data', Empty,
+        handle_clear_data)
     rospy.loginfo(
         "Ready to receive save image requests on /save_image_request.")
     rospy.spin()
