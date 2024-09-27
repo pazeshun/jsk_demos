e933d341f000d3ee970d3e2fd16e9ce54372e876
Iori Yanokura <ab.ioryz@gmail.com>
2024-09-27 12:17:56 +0900

    Add handeye calib program

---
+++ vzense_demo/scripts/handeye_calib.py
@@ -0,0 +1,124 @@
+#!/usr/bin/env python3
+
+import numpy as np
+import datetime
+import skrobot
+from skrobot.interfaces import PR2ROSRobotInterface as PR2Interface
+from skrobot.coordinates import make_coords
+
+import rospy
+
+from vzense_demo.k_arm import KARM
+from vzense_demo.k_arm_interface import KARMROSRobotInterface
+from skrobot.viewers import TrimeshSceneViewer
+
+import tf
+import tf2_ros
+
+
+class DataCollector(object):
+
+    def __init__(self):
+        self.tf_listener = tf.TransformListener()
+        self.filedata_cnt = 0
+        self.filedata = {}
+
+        self.base_frame_id = rospy.get_param('~base_frame_id', 'BODY')
+        self.ee_frame_id = rospy.get_param('~ee_frame_id', 'RARM_LINK3')
+        self.camera_frame_id = rospy.get_param('~camera_frame_id',
+                                               'rarm_hand_camera_link')
+        self.marker_frame_id = rospy.get_param('~marker_frame_id', '6x5chessboard')
+
+    def lookup_transform(self, from_frame_id, to_frame_id, stamp=None,
+                         timeout=4.0):
+        if stamp is None:
+            stamp = rospy.Time.now()
+        try:
+            self.tf_listener.waitForTransform(
+                from_frame_id, to_frame_id, stamp,
+                rospy.Duration(timeout))
+            (trans, rot) = self.tf_listener.lookupTransform(
+                from_frame_id, to_frame_id, stamp)
+        except (tf.LookupException,
+                tf.ConnectivityException,
+                tf.ExtrapolationException,
+                tf2_ros.LookupException,
+                tf2_ros.ConnectivityException,
+                tf2_ros.ExtrapolationException,
+                tf2_ros.TransformException,
+                rospy.exceptions.ROSTimeMovedBackwardsException):
+            return None
+        return (trans, rot)
+
+    def save_data(self, filename='/tmp/calib.yaml'):
+        import yaml
+        with open(filename, 'w') as f:
+            f.write(yaml.dump(self.filedata))
+
+    def store_data(self):
+        stamp = rospy.Time.now()
+        fdata = {}
+        fdata['stamp'] = float(rospy.Time.now().to_sec())
+        camera_to_marker_transform = self.lookup_transform(
+            self.camera_frame_id, self.marker_frame_id, stamp,
+            timeout=10.0)
+        base_to_ee_transform = self.lookup_transform(
+            self.base_frame_id, self.ee_frame_id, stamp)
+        if camera_to_marker_transform is None or \
+           base_to_ee_transform is None:
+            return False
+        fdata['camera_to_tag_transform'] = \
+            np.append(camera_to_marker_transform[0],
+                      camera_to_marker_transform[1]).tolist()
+        fdata['base_to_ee_transform'] = \
+            np.append(base_to_ee_transform[0],
+                      base_to_ee_transform[1]).tolist()
+        self.filedata[self.filedata_cnt] = fdata
+        self.filedata_cnt += 1
+
+
+r = KARM()
+for j in r.joint_list:
+    if j.max_joint_velocity == 0.0:
+        j.max_joint_velocity = np.deg2rad(5)
+ri = KARMROSRobotInterface(r)
+viewer = TrimeshSceneViewer()
+viewer.add(r)
+
+r.angle_vector(ri.angle_vector())
+ri.angle_vector(r.reset_pose(), 5.0)
+ri.wait_interpolation()
+
+
+dc = DataCollector()
+
+
+RARM_JOINT0_MIN = 0
+RARM_JOINT0_MAX = 0
+RARM_JOINT1_MIN = 0
+RARM_JOINT1_MAX = 0
+RARM_JOINT2_MIN = 0
+RARM_JOINT2_MAX = 0
+RARM_JOINT3_MIN = 0
+RARM_JOINT3_MAX = 0
+
+n_divide = 3
+n_pose = n_divide ** 4
+cnt = 0
+for angle0 in np.linspace(RARM_JOINT0_MIN, RARM_JOINT0_MAX):
+    r.RARM_JOINT0.joint_angle(angle0)
+    for angle1 in np.linspace(RARM_JOINT1_MIN, RARM_JOINT1_MAX):
+        r.RARM_JOINT1.joint_angle(angle1)
+        for angle2 in np.linspace(RARM_JOINT2_MIN, RARM_JOINT2_MAX):
+            r.RARM_JOINT2.joint_angle(angle2)
+            for angle3 in np.linspace(RARM_JOINT3_MIN, RARM_JOINT3_MAX):
+                r.RARM_JOINT3.joint_angle(angle3)
+                ri.angle_vector(r.angle_vector(), 5.0)
+                ri.wait_interpolation()
+                rospy.sleep(2.0)
+                dc.store_data()
+                estimated_finish_time = datetime.datetime.now() + \
+                    datetime.timedelta(seconds=(n_pose - cnt - 1) * 8)
+                print("estimated finish time {}".format(estimated_finish_time))
+                cnt += 1
+dc.save_data()
