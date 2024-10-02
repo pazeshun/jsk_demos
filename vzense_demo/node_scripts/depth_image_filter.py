663f030673b93fae0010feef1274cf1845b09194
Iori Yanokura <ab.ioryz@gmail.com>
2024-10-02 23:43:41 +0900

    Add

--- vzense_demo/node_scripts/depth_image_filter.py
+++ vzense_demo/node_scripts/depth_image_filter.py
@@ -54,7 +54,7 @@ class DepthImageFilter(ConnectionBasedTransport):
         mask[mask_idx] = 255
         mask = (mask).astype(np.uint8)
 
-        out_depth = np.zeros(depth.shape)
+        out_depth = np.full(depth.shape, np.nan)
         out_depth[np.where(mask == 255)] = depth[np.where(mask == 255)]
         out_depth = out_depth.astype(np.float32)
         # Convert back to ROS Image message
