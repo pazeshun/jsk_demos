cb207719becf30fad1dbe1d470505d399e572755
Iori Yanokura <ab.ioryz@gmail.com>
2024-10-07 23:49:15 +0900

    Add

--- vzense_demo/scripts/right_arm_calib_pose.py
+++ vzense_demo/scripts/right_arm_calib_pose.py
@@ -15,7 +15,7 @@ for j in robot_model.joint_list:
 viewer = PyrenderViewer()
 viewer.add(robot_model)
 viewer.show()
-ri = KARMROSRobotInterface(robot_model)
+ri = KARMROSRobotInterface(robot_model, use_hand=False)
 robot_model.angle_vector(ri.angle_vector())
 
 
@@ -23,8 +23,6 @@ calib_pose_angles = np.array([ 3.5281528e-02, -1.3015816e+00,  1.2076254e+00, -1
                                -1.1977290e-04, -3.0139906e-06,  3.9547164e-04, -1.5707964e+00,
                                0.0000000e+00,  0.0000000e+00,  0.0000000e+00], dtype=np.float32)
 robot_model.rarm.angle_vector(calib_pose_angles)
-
-robot_model.angle_vector(calib_pose_angles)
 
 yes_or_no = input('Move robot? [Y/n]').lower()
 if yes_or_no == 'y':
